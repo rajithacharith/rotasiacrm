d2f4b3cee70063e26a1933f4cbbf56e834645376
Ubuntu <azureuser@shopsite.3zesrqb03tou5ovvpbtewxlznb.bx.internal.cloudapp.net>
2023-12-21 15:55:50 +0000

    changes

---
+++ app/Orchid/Screens/TaskScreen.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use Orchid\Screen\Screen;
+
+class TaskScreen extends Screen
+{
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+	public function name(): ?string
+	{
+		return 'Simple To-Do List';
+	}
+	
+	/**
+	 * The description is displayed on the user's screen under the heading
+	 */
+	public function description(): ?string
+	{
+		return 'Orchid Quickstart';
+	}
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [];
+    }
+}
